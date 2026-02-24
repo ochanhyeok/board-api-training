0b3d7dbff90455360637b531065db6aace7d6a79
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-02-25 00:26:29 +0900

    feat: member엔티티 작성

---
+++ src/main/java/com/ochanhyeok/board/member/Member.java
@@ -0,0 +1,29 @@
+package com.ochanhyeok.board.member;
+
+import java.time.LocalDateTime;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String nickname;
+
+	private LocalDateTime createdAt;
+}
