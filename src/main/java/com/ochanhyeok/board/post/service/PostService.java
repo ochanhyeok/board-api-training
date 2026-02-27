7acb5e8af7e449418be6b630cba0b227b53aff69
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-02-27 10:51:07 +0900

    feat: PostService 구현

---
+++ src/main/java/com/ochanhyeok/board/post/service/PostService.java
@@ -0,0 +1,65 @@
+package com.ochanhyeok.board.post.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ochanhyeok.board.post.entity.Post;
+import com.ochanhyeok.board.post.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+	private final PostRepository postRepository;
+
+	/**
+	 * 게시글 작성
+	 */
+	@Transactional
+	public Post save(Post post) {
+		return postRepository.save(post);
+	}
+
+	/**
+	 * 게시글 목록
+	 */
+	@Transactional(readOnly = true)
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+
+	/**
+	 * 게시글 상세
+	 */
+	@Transactional
+	public Post findOne(Long id) {
+		Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
+		post.setViewCount(post.getViewCount() + 1);
+		return post;
+	}
+
+	/**
+	 * 게시글 수정
+	 */
+	@Transactional
+	public Post update(Long id, String newTitle, String newContent) {
+		Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
+		post.setTitle(newTitle);
+		post.setContent(newContent);
+		return post;
+	}
+
+	/**
+	 * 게시글 삭제
+	 */
+	@Transactional
+	public void delete(Long id) {
+		postRepository.deleteById(id);
+	}
+}
